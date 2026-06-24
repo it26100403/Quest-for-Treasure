ee37d90aa243155dfc070ee07bbe0bc2a41e0b9a
hasi <it26100403@my.sliit.lk>
2026-06-24 12:41:37 +0000

    Game

--- game.c
+++ game.c
@@ -1,57 +1,161 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define SIZE 15
-#define WALL '#'
-#define TREASURE 'T'
-#define HEALTH 'H'
-#define KEY 'K'
-#define DOOR 'D'
 
 char map[SIZE][SIZE];
 
+void initializeMap();
+void placewalls();
+void placeTreasure();
+void placeHealthPack();
+void placeKeys();
+void placeDoors();
+void printMap();
+
+int main()
+{
+    srand(time(NULL));
+    initializeMap();
+    placewalls();
+    placeTreasure();
+    placeHealthPack();
+    placeKeys();
+    placeDoors();
+    printMap();
+
+    return 0;
+}
+
 void initializeMap()
 {
+    int row, col;
 
-    for(int i=0;i<SIZE;i++)
+    for(row = 0; row < SIZE; row++)
     {
-        for(int j=0;j<SIZE;j++)
+        for(col = 0; col < SIZE; col++)
         {
-            map[i][j] = ' ';
+            map[row][col] = ' ';
+        }
+    }
+    // Border walls
+    for(row = 0; row < SIZE; row++)
+    {
+        map[row][0] = '#';
+        map[row][SIZE - 1] = '#';
+    }
+
+    for(col = 0; col < SIZE; col++)
+    {
+        map[0][col] = '#';
+        map[SIZE - 1][col] = '#';
+    }
+
+
+}
+
+void placewalls()
+{
+    int row, col;
+    int count = 0;
+
+    while(count<30)
+    {
+        row = rand() % SIZE;
+        col = rand() % SIZE;
+
+        if(map[row][col] == ' ')
+        {
+            map[row][col] = '#';
+            count++;
         }
     }
 }
 
-void createBorders()
+void placeTreasure()
 {
+   int row, col;
+    int count = 0;
 
-    for(int i=0;i<SIZE;i++)
+    while(count<12)
     {
-        map[0][i] = '#';
-        map[SIZE-1][i] = '#';
-        map[i][0] = '#';
-        map[i][SIZE-1] = '#';
+        row = rand() % SIZE;
+        col = rand() % SIZE;
+
+        if(map[row][col] == ' ')
+        {
+            map[row][col] = 'T';
+            count++;
+        }
+    }
+
+}
+
+void placeHealthPack()
+{
+    int row, col;
+    int count = 0;
+
+    while(count<5)
+    {
+        row = rand() % SIZE;
+        col = rand() % SIZE;
+
+        if(map[row][col] == ' ')
+        {
+            map[row][col] = 'H';
+            count++;
+        }
     }
 }
 
+void placeKeys()
+{
+    int row, col;
+    int count = 0;
+
+    while(count<3)
+    {
+        row = rand() % SIZE;
+        col = rand() % SIZE;
+
+        if(map[row][col] == ' ')
+        {
+            map[row][col] = 'K';
+            count++;
+        }
+    }
+}
+
+void placeDoors()
+{
+    int row, col;
+    int count = 0;
+
+    while(count<3)
+    {
+        row = rand() % SIZE;
+        col = rand() % SIZE;
+
+        if(map[row][col] == ' ')
+        {
+            map[row][col] = 'D';
+            count++;
+        }
+    }
+}
 void printMap()
 {
+    int row, col;
 
-    for(int i=0;i<SIZE;i++)
+    for(row = 0; row < SIZE; row++)
     {
-        for(int j=0;j<SIZE;j++)
+        for(col = 0; col < SIZE; col++)
         {
-            printf("%c ", map[i][j]);
+            printf("%c ", map[row][col]);
         }
 
         printf("\n");
     }
-}
-
-int main()
-{
-    initializeMap();
-    createBorders();
-    printMap();
-
-    return 0;
 }
